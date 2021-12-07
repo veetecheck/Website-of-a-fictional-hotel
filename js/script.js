9253edc1396111a4611e87df645b83979a11df66
veetecheck <93823516+veetecheck@users.noreply.github.com>
2021-12-07 17:50:14 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
 $(function (){
     $('[data-toggle="tooltip"]').tooltip();
   });
-$(function () {
-    $('[data-toggle="popover"]').popover()
-  })
 
-$('.modal').on('hide.bs.modal', function () {
+  $('.modal').on('hide.bs.modal', function () {
     var memory = $(this).html();
     $(this).html(memory);
 });
